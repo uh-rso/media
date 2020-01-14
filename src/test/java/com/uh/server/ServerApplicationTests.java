b811fb3a503ddb874d240b0b20f17781dc6497db
Uroš Hekić <uros.hekic@marand.si>
2020-01-14 15:03:55 +0100

    Make context load again

--- src/test/java/com/uh/server/ServerApplicationTests.java
+++ src/test/java/com/uh/server/ServerApplicationTests.java
@@ -4,12 +4,16 @@ import com.thebuzzmedia.exiftool.ExifTool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class ServerApplicationTests {
 
 	@MockBean
 	private ExifTool exifTool;
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
